0d7c0f66c65f3899eee08a35c8f00c6b672e4cbd
KuraiRantan <jorgecamargo2012902@gmail.com>
2024-03-17 04:10:29 -0500

    feat: Add Docker support and configuration.

---
+++ entry_point.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Instalar requisitos
+pip3 install -r ./applications/SGE/requirements.txt
+
+# Ejecutar migraciones de la base de datos
+cd ./applications/SGE
+python -m alembic upgrade head
+
+# Iniciar la aplicación
+cd ../..
+./web2py.py -i 0.0.0.0 -a "${PASSWORD_ADMIN_SERVER}" -c ./ssl/server.crt -k ./ssl/server.key
