78c8e6d6732686aeae8b5563c49f0137c4932c4f
KuraiRantan <jorgecamargo2012902@gmail.com>
2024-03-28 22:59:09 -0500

    Doc, Refactor: Added docstrings and type annotations. The get_all method is modified so that it returns a list of instances of the Classroom class.

--- modules/repository/classroom_repository.py
+++ modules/repository/classroom_repository.py
@@ -1,12 +1,31 @@
-class ClassroomRepository(object):
+from typing import List
+from gluon.dal import DAL
+from modules.models.classroom import Classroom
+from modules.factory.classroom_factory import ClassroomFactory
 
-    def __init__(self, db) -> None:
+class ClassroomRepository(object):
+    """Class that provides methods to interact with the database, specifically with the Classroom entity."""
+    
+    def __init__(self, db: DAL) -> None:
+        """Constructor of the ClassroomRepository class.
+
+        Args:
+            db (DAL): The database connection instance.
+        """
         self._db = db
         self._classroom_table = db.classroom
 
-    def get_all(self):
+    def get_all(self) -> List[Classroom]:
+        """Get all the classrooms.
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            List[Classroom]: List of all classrooms.
+        """
         try:
             classrooms = self._db(self._classroom_table.id > 0).select()
-            return classrooms
+            return [ClassroomFactory.list(**classroom.as_dict()) for classroom in classrooms]
         except Exception as e:
             raise Exception('Classrooms could not be obtained.', e)
