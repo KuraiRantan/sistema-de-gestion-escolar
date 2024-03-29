9c42134994d9eab9d659b27642f29e684744287f
KuraiRantan <jorgecamargo2012902@gmail.com>
2024-03-28 23:03:17 -0500

    Doc: Added docstrings and type annotations for repository classes: Student and Subject.

--- modules/repository/student_repository.py
+++ modules/repository/student_repository.py
@@ -1,38 +1,91 @@
+from typing import Union, List
+from gluon.dal import DAL
+from modules.models.student import Student
+from modules.factory.student_factory import StudentFactory
+from modules.models.definitions import Gender, TypeOfIdentifications
+
 class StudentRepository(object):
+    """Class that provides methods to interact with the database, specifically with the Student entity."""
 
-    def __init__(self, db) -> None:
+    def __init__(self, db: DAL) -> None:
+        """Constructor of the StudentRepository class.
+
+        Args:
+            db (DAL): The database connection instance.
+        """
         self._db = db;
-        self._student_table = db.student;
+        self._student_table = db.student
 
-    def add(self, student):
+    def add(self, student: Student) -> Student:
+        """Insert a new student into the database.
+
+        Args:
+            student (Student): The instance that contains the new student information.
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            Student: The instance that contains the information for the new student added.
+        """
         try:
             id = self._student_table.insert(**student.as_dict())
-            student = self._db(self._student_table.id == id).select().first()
+            student.id = id
             return student
         except Exception as e:
             raise Exception('Error adding student.', e)
     
-    def update(self, student):
+    def update(self, student: Student) -> Union[Student, None]:
+        """Update information for an existing student.
+
+        Args:
+            student (Student): The instance that contains the student information.
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            Student | None: The instance containing the updated student information or None if the student does not exist.
+        """
         try:
-            student = self._db(self._student_table.id == student.id).select().first()
-            if(student):
-                student.update(**student.as_dict(include_none_data=True))
+            student_exist = self._db(self._student_table.id == student.id).select().first()
+            if(student_exist):
+                student_exist.update(**student.as_dict())
                 return student
             else:
                 return None
         except Exception as e:
             raise Exception('Error updating student.', e)
     
-    def get_grades(self):
+    def get_grades(self) -> List[str]:
+        """List of existing degrees (without repeating).
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            List[str]: List of degrees.
+        """
         try:
             grades = self._db(self._student_table.id>0).select(self._student_table.current_grade, distinct=True)
             return grades
         except Exception as e:
             raise Exception('Error fetching the grades.', e)
         
-    def get_all_by_grade(self, grade):
+    def get_all_by_grade(self, grade: str) -> List[Student]:
+        """Get all students by grade.
+
+        Args:
+            grade (str): Grade a of students to look for.
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            List[Student]: List of students by grade.
+        """
         try:
             students = self._db(self._student_table.current_grade == grade).select()
-            return students
+            return [StudentFactory.list(**student.as_dict()) for student in students]
         except Exception as e:
             raise Exception('Failed to retrieve the student filtered by grade.', e)
--- modules/repository/subject_repository.py
+++ modules/repository/subject_repository.py
@@ -1,12 +1,31 @@
-class SubjectRepository(object):
+from typing import List
+from gluon.dal import DAL
+from modules.models.subject import Subject
+from modules.factory.subject_factory import SubjectFactory
 
-    def __init__(self, db) -> None:
+class SubjectRepository(object):
+    """Class that provides methods to interact with the database, specifically with the Subject entity."""
+
+    def __init__(self, db: DAL) -> None:
+        """Constructor of the SubjectRepository class.
+        
+        Args:
+            db (DAL): The database connection instance.
+        """
         self._db = db
         self._subject_table = db.subject
 
-    def get_all(self):
+    def get_all(self) -> List[Subject]:
+        """Get all subjects.
+
+        Raises:
+            Exception: Error trying to perform the operation.
+
+        Returns:
+            list[Subject]: List of all subjects.
+        """
         try:
             subjects = self._db(self._subject_table.id > 0).select()
-            return subjects
+            return [SubjectFactory.list(**subject.as_dict()) for subject in subjects]
         except Exception as e:
             raise Exception('An error occurred while trying to retrieve the subjects.', e)
